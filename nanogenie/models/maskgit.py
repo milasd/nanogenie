02bfa467830cdf354254e58f32649b9fbefc77d6
milasd <milasoaresdeoliveira@gmail.com>
2025-11-27 21:40:48 -0300

    Add todo list to readme.md

--- nanogenie/models/maskgit.py
+++ nanogenie/models/maskgit.py
@@ -1,8 +1,9 @@
 import torch
 
+VOCAB_SIZE = 
 class MaskGIT:
     @staticmethod
-    def apply_masking(z: torch.Tensor, mask_ratio: float, mask_token_id) -> tuple[torch.Tensor, torch.Tensor]:
+    def apply_masking(z: torch.Tensor, mask_ratio: float, mask_token_id: int = VOCAB_SIZE) -> tuple[torch.Tensor, torch.Tensor]:
         """
         Apply random token masking for MaskGIT training. 
 
@@ -15,4 +16,6 @@ class MaskGIT:
             z_masked: Tokens post-masking (some replaced by mask_token_id)
             mask: Mask w/ booleans showing which were masked.
         """
-        pass
+        # Keep frame 1 unmasked; mask 2 to T-1 randomly.
+
+
